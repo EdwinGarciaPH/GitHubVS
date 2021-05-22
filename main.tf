6e93e5eac67c1dabc9f126bed4727da55b9edb47
Edwin Santos Garcia <Edwin_GarciaPH@outlook.com>
2021-05-22 14:24:24 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
 provider "aws" {
   region     = "ap-southeast-1"		
-  access_key = "AKIAXVIBKYT6O2KSD477"
-  secret_key = "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"
+  access_key = "AKIAXVIBKYT6L4UIQ7EA"
+  secret_key = "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"
+}
+
+provider "aws" {
+  region     = "ap-southeast-2"		
+  access_key = "AKIAXVIBKYT6L4UIQ7EA"
+  secret_key = "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"
+  alias = "apsouth2"
 }
 
 resource "aws_instance" "apsoutheast1" {
   ami           = "ami-02f26adf094f51167" 
   instance_type = "t2.micro"
+}
+
+resource "aws_instance" "apsoutheast2" {
+  ami           = "ami-0186908e2fdeea8f3" 
+  instance_type = "t2.micro"
+  provider = aws.apsouth2
 }
